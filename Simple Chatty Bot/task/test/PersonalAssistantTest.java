2fe86c39a77780e0075fb3be7641d28e10e8b817
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-04-08 02:31:34 +0530

    end commit

--- Simple Chatty Bot/task/test/PersonalAssistantTest.java
+++ Simple Chatty Bot/task/test/PersonalAssistantTest.java
@@ -9,11 +9,13 @@ import java.util.List;
 class Clue {
     int age;
     String name;
+        int count;
 
-    Clue(String name, int age) {
-        this.age = age;
-        this.name = name;
-    }
+        Clue(String name, int age, int count) {
+                this.age = age;
+                this.name = name;
+                this.count = count;
+        }
 }
 
 
@@ -25,31 +27,37 @@ public class PersonalAssistantTest extends BaseStageTest<Clue> {
 
     @Override
     public List<TestCase<Clue>> generate() {
-        return List.of(
-                new TestCase<Clue>()
-                        .setInput("John\n1 2 1")
-                        .setAttach(new Clue("John", 22)),
+            String input = "Marry\n1 0 5\n10";
 
-                new TestCase<Clue>()
-                        .setInput("Nick\n2 0 0")
-                        .setAttach(new Clue("Nick", 35))
-        );
+            for (int i = 1; i < 9; i++) {
+                    input += "\n" + i;
+            }
+
+            return List.of(
+                    new TestCase<Clue>()
+                            .setInput(input)
+                            .setAttach(new Clue("Marry", 40, 10))
+            );
     }
 
     @Override
     public CheckResult check(String reply, Clue clue) {
 
-        String[] lines = reply.trim().split("\n");
+            String[] lines = reply.trim().split("\n");
 
-        if (lines.length != 7) {
-            return CheckResult.FALSE(
-                    "You should output 7 lines. Lines found: " + lines.length + "\n" +
-                            "Your output:\n" +
-                            reply
-            );
-        }
+            int length = 9 + clue.count + 1;
 
-        String lineWithName = lines[3].toLowerCase();
+            if (lines.length <= length) {
+                    return CheckResult.FALSE(
+                            "You should output at least " + (length + 1) + " lines " +
+                                    "(for the count number " + clue.count + ").\n" +
+                                    "Lines found: " + lines.length + "\n" +
+                                    "Your output:\n" +
+                                    reply
+                    );
+            }
+
+            String lineWithName = lines[3].toLowerCase();
         String name = clue.name.toLowerCase();
 
         if (!lineWithName.contains(name)) {
@@ -64,16 +72,41 @@ public class PersonalAssistantTest extends BaseStageTest<Clue> {
         String lineWithAge = lines[6].toLowerCase();
         String age = Integer.toString(clue.age);
 
-        if (!lineWithAge.contains(age)) {
-            return CheckResult.FALSE(
-                    "Can't find a correct age " +
-                            "in the last line of output! " +
-                            "Maybe you calculated the age wrong?\n\n" +
-                            "Your last line: \n" + "\"" + lines[6] + "\""
-            );
-        }
+            if (!lineWithAge.contains(age)) {
+                    return CheckResult.FALSE(
+                            "Can't find a correct age " +
+                                    "in the last line of output! " +
+                                    "Maybe you calculated the age wrong?\n\n" +
+                                    "Your last line: \n" + "\"" + lines[6] + "\""
+                    );
+            }
 
-        return CheckResult.TRUE;
+            for (int i = 0; i < clue.count + 1; i++) {
+                    String numLine = lines[i + 8];
+                    String actualNum = i + "!";
+
+                    if (!numLine.equals(actualNum)) {
+                            return CheckResult.FALSE(
+                                    "Expected " + (i + 8) + "-th line: \n" +
+                                            "\"" + actualNum + "\"\n" +
+                                            "Your " + (i + 8) + "-th line: \n" +
+                                            "\"" + numLine + "\""
+                            );
+                    }
+            }
+
+            String lastLine = lines[lines.length - 1];
+
+            if (!lastLine.equals("Congratulations, have a nice day!")) {
+                    return CheckResult.FALSE(
+                            "Your last line should be:\n" +
+                                    "\"Congratulations, have a nice day!\"\n" +
+                                    "Found:\n" +
+                                    "\"" + lastLine + "\""
+                    );
+            }
+
+            return CheckResult.TRUE;
     }
 
 }
